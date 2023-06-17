748a06c13975213eab9c772d530c8d922e0b962f
Muhammad Alfi <125952078+allcuy@users.noreply.github.com>
2023-06-17 10:41:00 +0700

    Add files via upload

---
+++ percabangan.php
@@ -0,0 +1,13 @@
+<?php
+
+$nilai = 75; //nilai yang akan di uji
+$nilsi = 60;
+
+if($nilai >= 60 && $nilai <75) {
+    echo "Selamat, anda lulus dengan baik";
+    echo "<br/>";
+}if($nilai >=75 && $nilai <50>) {
+    echo "Selamat, anda lulus dengan baik";
+} else {
+    echo "Maaf, anda belum lulus";
+} 
