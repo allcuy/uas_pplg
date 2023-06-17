ae97fac107d73c3e5c0b01d6984ee6ff42b135da
Muhammad Alfi <125952078+allcuy@users.noreply.github.com>
2023-06-17 10:39:39 +0700

    Add files via upload

---
+++ function2.php
@@ -0,0 +1,16 @@
+<?php
+// Membuat fungsi
+function perkalian ($a, $b)
+{
+    $hasil = $a * $b;
+    return $hasil;
+}
+//Memanggil fungsi
+$hasil = perkalian (3, 4);
+echo $hasil ; // hasil 12
+echo "<br/>";
+
+// Memanggil fungsi
+$a = 4;
+$b = 5;
+echo "hasil perkalian" . $a . "x" .$b . "adalah" .perkalian ($a, $b)
