b46036f5e7436f08d46ccc1589aeefea13a29b8e
Muhammad Alfi <125952078+allcuy@users.noreply.github.com>
2023-06-17 10:39:08 +0700

    Add files via upload

---
+++ function.php
@@ -0,0 +1,23 @@
+<?php
+    function salam  () {
+        echo 'selamat siang XRPL1';
+    }
+
+    salam();
+
+
+    function datadiri () {
+        $nama = 'al';
+        $sekolah = 'SMKN 1';
+        echo 'saya adalah' .$nama. 'dan saya sekolah di' .$sekolah;
+    }
+
+    function perjumlahan () {
+        $b=5;
+        $c=6; 
+        $b=5;
+    }
+
+
+    echo '</br' ;
+    datadiri () ;
